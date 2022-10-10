7e5c4a329d82a7c06f22d731850dadef60ddeb06
Afrim Avmedoski <afrim.avmedoski@gmail.com>
2022-10-10 21:59:07 +0200

    1 Predavanja

---
+++ Domače Naloge/DN01 - Strelske Vaje/strelske_vaje.py
@@ -0,0 +1,20 @@
+# Rok za oddajo: sreda, 12. oktober 2022, 11.15
+
+"""
+Napiši program za izračun dolžine strela s topom (ki brez trenja izstreljuje točkaste krogle v brezzračnem prostoru,
+a pustimo trivio). Program od uporabnika ali uporabnice zahteva, da mu ta vpiše hitrost izstrelka in kot, pod katerim
+je izstreljena (krogla, ne uporabnica). Program izračuna in izpiše, kako daleč bo letela krogla.
+
+Pomoč za fizično nebogljene: s=v2sin2ϕg, kjer je s razdalja, v hitrost izstrelka, ϕ je kot, g pa osma črka slovenske
+abecede.
+
+Preverite tole: krogla leti najdalj, če jo izstrelimo pod kotom 45 stopinj. Poskusite, kako daleč gre pod kotom 45 in
+kako daleč pod 46 stopinj - po 45 mora leteti dlje. In če pod kotom 50 stopinj leti nazaj (razdalja je negativna), ste
+ga gotovo nekje polomili. Kaj, točno, je narobe, vam lahko hitro sčveka kolega, ki je že rešil problem, vendar vam
+priporočam, da ga poskusite odkriti sami - za vajo v sledenju programu. Za namig lahko poškilite na del Wikipedijine
+strani o kotih.
+
+(Če je g enak 9.807, mora krogla, izstreljena z 10 m/s po kotom 45 stopinj preleteti približno 10.196798205363516
+metrov. Če je g 10, pa 10 metrov.)
+"""
+
---
+++ Predavanja/01 izrazi, spremenljivke, program/temperature.py
@@ -0,0 +1,6 @@
+# Prvi čisto pravi program
+
+temp_C = float(input("Temperatura [C°]? "))
+temp_K = temp_C + 273.15
+temp_F = temp_C * 5/9 + 32
+print(temp_C, "C° je enako", temp_K, "K ali", temp_F, "F")
