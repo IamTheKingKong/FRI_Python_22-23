7466165feb8fe25e085bdd7a7ede5e9f0b87638e
Afrim Avmedoski <afrim.avmedoski@gmail.com>
2022-10-11 20:15:58 +0200

    11.10.2022 - Rešitev Naloge

--- Domače Naloge/DN01 - Strelske Vaje/strelske_vaje.py
+++ Domače Naloge/DN01 - Strelske Vaje/strelske_vaje.py
@@ -1,12 +1,10 @@
+import math
 # Rok za oddajo: sreda, 12. oktober 2022, 11.15
 
 """
 Napiši program za izračun dolžine strela s topom (ki brez trenja izstreljuje točkaste krogle v brezzračnem prostoru,
 a pustimo trivio). Program od uporabnika ali uporabnice zahteva, da mu ta vpiše hitrost izstrelka in kot, pod katerim
 je izstreljena (krogla, ne uporabnica). Program izračuna in izpiše, kako daleč bo letela krogla.
-
-Pomoč za fizično nebogljene: s=v2sin2ϕg, kjer je s razdalja, v hitrost izstrelka, ϕ je kot, g pa osma črka slovenske
-abecede.
 
 Preverite tole: krogla leti najdalj, če jo izstrelimo pod kotom 45 stopinj. Poskusite, kako daleč gre pod kotom 45 in
 kako daleč pod 46 stopinj - po 45 mora leteti dlje. In če pod kotom 50 stopinj leti nazaj (razdalja je negativna), ste
@@ -18,3 +16,11 @@ strani o kotih.
 metrov. Če je g 10, pa 10 metrov.)
 """
 
+hitrost_izstrelka = float(input("Vnesite hitrost izstrelka: "))
+kot_izstrelka = float(input("Vnesite kot izstrelka: "))
+g = 9.807
+
+#https://physics.stackexchange.com/questions/107933/how-to-calculate-distance-travelled-from-velocity-vector-and-angle
+dolzina_strela = (hitrost_izstrelka**2 * math.sin(2 * math.radians(kot_izstrelka))) / g
+
+print("Dolžina strela s topom je: " + str(dolzina_strela) + " m")
